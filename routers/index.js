749aa392d3474fb4761d8d0a781ff8594720c1c9
Kevinnan-teen <Kevinnan813@gmail.com>
2022-04-27 18:59:42 +0800

    路由汇总

---
+++ routers/index.js
@@ -0,0 +1,22 @@
+const Router = require('koa-router');
+
+const home = require('./home');
+const signin_router = require('./signin');
+const signup_router = require('./signup');
+const signout_router = require('./signout');
+const profile_router = require('./profile');
+const apply_anchor_router = require('./apply_anchor');
+const room = require('./room');
+
+
+//装载所有子路由
+let router = new Router();
+router.use('/', home.routes(), home.allowedMethods());
+router.use('/', signin_router.routes(), signin_router.allowedMethods());
+router.use('/', signup_router.routes(), signup_router.allowedMethods());
+router.use('/', signout_router.routes(), signout_router.allowedMethods());
+router.use('/', profile_router.routes(), profile_router.allowedMethods());
+router.use('/', apply_anchor_router.routes(), apply_anchor_router.allowedMethods());
+router.use('/', room.routes(), room.allowedMethods());
+
+module.exports = router;
